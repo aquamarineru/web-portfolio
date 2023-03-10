5c2c00ea24289a0f224b45922262e0381a061ca6
Marina Ilyushina <m_ilyushina@hotmail.com>
2023-03-10 17:25:09 +0100

    workitem a

--- src/components/work/WorksItem.jsx
+++ src/components/work/WorksItem.jsx
@@ -8,10 +8,10 @@ export default function WorksItem({item}) {
         <img src={item.image} alt="work" className="work__img" />
         <h3 className="work__title">{item.title}</h3>
         <div className="work__btns">
-          <a href={item.url} className="work__button"> Demo
+          <a href={item.url} className="work__button" target="_blank" > Demo
               <AiOutlineArrowRight className="work__button-icon" />
           </a>
-          <a href={item.github} className="work__github work__button">
+          <a href={item.github} className="work__github work__button" target="_blank" >
               <FiGithub className="work__github-icon" />
               <AiOutlineArrowRight className="work__icon work__button-icon" />
           </a>
